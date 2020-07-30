36e2d89693f01449c8574bdbded28d1d4f10c02d
RANGERBAWA <38451055+RANGERBAWA@users.noreply.github.com>
2020-07-30 17:42:03 +0530

    Add files via upload

---
+++ RANGER.cpp
@@ -0,0 +1,56 @@
+                     /*~~~~~~~~~~~~~~~~~  AUTHOR == DIKSHU SHARMA ~~~~~~~~~~~~~~~~~~~*/
+/*~~~~~~~~~~~~~~~~~  TO THE WELL-ORGANIZED MIND, DEATH IS BUT THE NEXT GREAT ADVENTURE  ~~~~~~~~~~~~~~~~~~~~*/
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~``````~~~ LIVE IN THE PRESENT MOMENT ~~~``````~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ FIRST SOLVE THE PROBLEM THEN WRITE THE CODE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+#include "bits/stdc++.h"
+using namespace std;
+#define lli long long int
+#define endl "\n"
+#define fr(i, a, b) for(int i = a; i < b; i++)
+#define rf(i, a, b) for(int i = a-1; i >= b; i--)
+#define pf push_front
+#define pb push_back
+#define eb emplace_back
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)x.size()
+#define lb lower_bound
+#define ub upper_bound
+
+const lli       MOD=1000000007;
+
+
+int main() 
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+    lli t;
+    cin >> t;
+    while(t--) 
+    {
+    	lli a[1000000],b[1000000]={0},c[1000000]= {0};
+    	lli i=0,j=0,k=0,ii=0,jj=0,kk=0;
+    	lli x=0,y=0,z=0,xx=0,yy=0,zz=0;
+    	lli maxxi = 0, minni = INT_MAX , ans = 0, flag=0 , flag1 = 0,flag2=0,num=0,temp=0;
+    	lli n,m,l=0,r=0,freq=0,left=0,right=0,odd=0,even=0,pro=1,sum=0,index=0,diff=0;
+    	map<lli,lli>messi;
+    	vector<lli> vex;
+    	stack<lli> stick;
+    	queue<lli> queen;
+    	deque<lli>dan;
+    	set<lli> sea;
+    	pair<lli,lli> pool;
+    	string s;
+
+ 		vector<lli>::iterator it;
+
+
+
+
+
+    }
+
+    return 0;
+}
