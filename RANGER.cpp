747d77d249a8e05dde5caaa189db9a6dc778bcac
RANGERBAWA <38451055+RANGERBAWA@users.noreply.github.com>
2020-08-17 01:38:51 +0530

    Update RANGER.cpp

--- RANGER.cpp
+++ RANGER.cpp
@@ -1,40 +1,58 @@
-                     /*~~~~~~~~~~~~~~~~~  AUTHOR == DIKSHU SHARMA ~~~~~~~~~~~~~~~~~~~*/
-/*~~~~~~~~~~~~~~~~~  TO THE WELL-ORGANIZED MIND, DEATH IS BUT THE NEXT GREAT ADVENTURE  ~~~~~~~~~~~~~~~~~~~~*/
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~``````~~~ LIVE IN THE PRESENT MOMENT ~~~``````~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ FIRST SOLVE THE PROBLEM THEN WRITE THE CODE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+               /*~~~~~~~~~~~~  AUTHOR == DIKSHU SHARMA  ~~~~~~~~~~~~~*/
+/*~~~~~~~~ TO THE WELL-ORGANIZED MIND, DEATH IS BUT THE NEXT GREAT ADVENTURE  ~~~~~~~~~~*/
+/*~~~~~~~~~~~~~~~~~``````~~~ LIVE IN THE PRESENT MOMENT ~~~``````~~~~~~~~~~~~~~~~~*/
+/*~~~~~~~~~~~~~~~~~~ FIRST SOLVE THE PROBLEM THEN WRITE THE CODE ~~~~~~~~~~~~~~~~~~~~~~~*/
 
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 using namespace std;
 #define lli long long int
 #define endl "\n"
-#define fr(i, a, b) for(int i = a; i < b; i++)
-#define rf(i, a, b) for(int i = a-1; i >= b; i--)
-#define pf push_front
-#define pb push_back
-#define eb emplace_back
 #define F first
 #define S second
-#define all(x) x.begin(), x.end()
-#define sz(x) (int)x.size()
 #define lb lower_bound
 #define ub upper_bound
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define tr(x,it) for(auto it = (x).begin();it!=(x).end();++it)
+#define rtr(x,it) for(auto it = (x).rbegin();it!=(x).rend();++it)
+#define ufy(v) sort(all(v));(v).erase(unique(all((v))),(v).end())
+#define sz(a) int((a).size()) 
+#define boost ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define pb push_back 
+#define PB pop_back
+#define pf push_front
+#define PF pop_front
+#define MP make_pair
+#define clr clear
+#define rz resize
+#define mset(a,b) memset(a,b,sizeof(a))
+#define ia(a,n) FOR(i,0,n-1)cin>>a[i]
+#define ia1(a,n) FOR(i,1,n)cin>>a[i]
+#define fpresent(c,x) ((c).find(x) != (c).end())  // set,map
+#define present(c,x) (find(all(c),x) != (c).end())  //vector
+#define FOR(i,a,b) for(int i=a;i<=b;++i)
+#define RFOR(i,a,b) for(int i=a;i>=b;--i)
+#define rep(i,n) FOR(i,0,n-1)
+#define maxn 1000000
+#define MOD 1000000007;
 
-const lli       MOD=1000000007;
 
 
 int main() 
 {
-    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    boost;
 
     lli t;
     cin >> t;
+
     while(t--) 
     {
     	lli a[1000000],b[1000000]={0},c[1000000]= {0};
     	lli i=0,j=0,k=0,ii=0,jj=0,kk=0;
     	lli x=0,y=0,z=0,xx=0,yy=0,zz=0;
-    	lli maxxi = 0, minni = INT_MAX , ans = 0, flag=0 , flag1 = 0,flag2=0,num=0,temp=0;
+    	lli maxxi = INT_MIN, minni = INT_MAX , ans = 0, flag=0 , flag1 = 0,flag2=0,num=0,temp=0;
     	lli n,m,l=0,r=0,freq=0,left=0,right=0,odd=0,even=0,pro=1,sum=0,index=0,diff=0;
+    	char ch,cchh,ccchhh;
     	map<lli,lli>messi;
     	vector<lli> vex;
     	stack<lli> stick;
@@ -45,7 +63,8 @@ int main()
     	string s;
 
  		vector<lli>::iterator it;
-
+ 		vector<lli>::reverse_iterator itr;
+ 		
 
 
 
